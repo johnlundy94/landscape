f3cdc2f42b1f3f04190493cb9f5587e1e7e15852
John Lundy <johnclundyy@gmail.com>
2024-01-09 15:01:48 -0700

    Navbar basic styling complete

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,21 +14,12 @@ function Hero() {
       <img src={landscape4} alt="Fourth" className="image-four col-2-row-3" />
       <img src={landscape5} alt="Fifth" className="image-five col-1-row-2" />
       <h1 className="text-span-2">Collect your best articles easily</h1>
-      {/* <div className="hero-subtext">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis
-          egestas maecenas pharetra convallis. Eget mi proin sed libero enim sed
-          faucibus. Enim facilisis gravida neque convallis a cras semper auctor.
-          Morbi enim nunc faucibus a pellentesque sit amet. Tortor dignissim
-          convallis aenean et tortor at risus viverra. Nisl pretium fusce id
-          velit ut tortor. Pellentesque habitant morbi tristique senectus et
-          netus et. Velit sed ullamcorper morbi tincidunt ornare massa eget
-          egestas. Justo eget magna fermentum iaculis eu non. At risus viverra
-          adipiscing at in tellus integer feugiat. In eu mi bibendum neque
-          egestas congue. Amet commodo nulla facilisi nullam.
-        </p>
-      </div> */}
+      <p className="sub-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Turpis egestas
+        maecenas pharetra convallis. Eget mi proin sed libero enim sed faucibus.
+        Enim facilisis gravida neque convallis a cras semper auctor.
+      </p>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,38 @@
+import { useState } from "react";
 import "../styles/Nav.css";
 import { Link } from "react-scroll";
+import Hamburger from "../assets/hamburger.svg";
 
 function Nav() {
+  const [expand, setExpand] = useState(false);
+
   return (
     <div className="Nav">
       <div className="brand-logo">Logo</div>
-      <div className="nav-buttons">
-        <Link to="hero" smooth={true} duration={1000}>
+      <div className="hamburger" onClick={() => setExpand(!expand)}>
+        <img src={Hamburger} alt="Menu" />
+      </div>
+      <div className={`nav-buttons ${expand ? "expanded" : ""}`}>
+        <Link to="hero" smooth={true} duration={1000} className="link">
           Home
         </Link>
-        <Link to="gallery" smooth={true} duration={1000}>
+        <Link to="gallery" smooth={true} duration={1000} className="link">
           Gallery
         </Link>
-        <Link to="services" smooth={true} duration={1000}>
+        <Link to="services" smooth={true} duration={1000} className="link">
           Services
         </Link>
-        <Link to="about" smooth={true} duration={1000}>
+        <Link to="about" smooth={true} duration={1000} className="link">
           About
         </Link>
-        <Link to="testimonials" smooth={true} duration={1000}>
+        <Link to="testimonials" smooth={true} duration={1000} className="link">
           Testimonials
         </Link>
-        <Link to="contact" smooth={true} duration={1000}>
+        <Link to="contact" smooth={true} duration={1000} className="link">
           Contact
         </Link>
-        <Link to="calculator" smooth={true} duration={1000}>
-          calculator
+        <Link to="calculator" smooth={true} duration={1000} className="link">
+          Calculator
         </Link>
       </div>
     </div>
