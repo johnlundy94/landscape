06d32bf860817292dd9ba8e4546e41fea3877ef4
John Lundy <johnclundyy@gmail.com>
2024-01-25 13:31:16 -0700

    Added MUI buttons for previous and next methods

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,56 @@
+import { useState } from "react";
 import "../styles/Hero.css";
+import Slider from "./Slider";
 import landscape1 from "../assets/landscape1.png";
 import landscape2 from "../assets/landscape2.png";
 import landscape3 from "../assets/landscape3.png";
 import landscape4 from "../assets/landscape4.png";
 import landscape5 from "../assets/landscape5.png";
+import { Button } from "@mui/material";
 
 function Hero() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const landscapes = [
+    landscape1,
+    landscape2,
+    landscape3,
+    landscape4,
+    landscape5,
+  ];
+
+  const nextSlide = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === landscapes.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevSlide = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === 0 ? landscapes.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div className="hero-container">
       <h1 className="text-span-2">Company Name</h1>
-      <div className="slider">
-        <img src={landscape1} alt="First" className="image-one" />
-        <img src={landscape2} alt="Second" className="image-two" />
-        <img src={landscape3} alt="Third" className="image-three" />
-        <img src={landscape4} alt="Fourth" className="image-four" />
-        <img src={landscape5} alt="Fifth" className="image-five" />
+      <div className="slider-container">
+        {landscapes.map((landscape, index) => (
+          <Slider
+            key={index}
+            inProp={activeIndex === index}
+            direction="left"
+            className="slider"
+          >
+            <img src={landscape} alt={`Slide ${index}`} />
+          </Slider>
+        ))}
       </div>
+      <Button variant="text" onClick={prevSlide}>
+        Previous
+      </Button>
+      <Button variant="text" onClick={nextSlide}>
+        Next
+      </Button>
     </div>
   );
 }
