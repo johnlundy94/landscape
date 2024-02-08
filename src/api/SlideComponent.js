27a61161d251a22542f6b932f8bdc15d794b944d
John <johnclundyy@gmail.com>
2024-02-08 10:26:53 -0700

    Slider is at a good stoppoing point

--- src/api/SlideComponent.js
+++ src/api/SlideComponent.js
@@ -1,14 +1,12 @@
 import Slide from "@mui/material/Slide";
 import "../styles/Hero.css"
 
-const SlideComponent = ({ isActive, direction, imgSrc, children }) => {
+const SlideComponent = ({ isActive, direction, children }) => {
   return (
     <Slide direction={direction} in={isActive} timeout={{enter: 1000, exit: 1000}} >
       <div className="slider-content">
-        <img src={imgSrc} alt="Slide image" className="slider-image"/>
-        {children}
-      </div>
-
+      {children}
+        </div>
     </Slide>
   );
 };
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,17 +7,19 @@ import landscape3 from "../assets/landscape3.png";
 import landscape4 from "../assets/landscape4.png";
 import landscape5 from "../assets/landscape5.png";
 import { Button } from "@mui/material";
+import { Link } from 'react-router-dom'
+
 
 function Hero() {
 
   const [activeIndex, setActiveIndex] = useState(0);
 
   const landscapes = [
-    landscape1,
-    landscape2,
-    landscape3,
-    landscape4,
-    landscape5,
+    {name: 'Serenity Gardens', image: landscape1},
+    {name: 'Elysian Fields',image: landscape2},
+    {name: 'Arcadian Retreat',image: landscape3},
+    {name: 'Eden Enclave',image: landscape4},
+    {name: 'Oasis Estates',image: landscape5},
   ];
 
   const nextSlide = () => {
@@ -35,20 +37,30 @@ function Hero() {
   return (
     <div className="hero-container">
       <h1 className="text-span-2">Our Portfolio</h1>
-      <div className="slider-container"  id="properties" >
+      <div className="slider-container" id="properties" >
         {landscapes.map((landscape, index) => (
-          <SlideComponent className="slider" isActive={activeIndex === index} direction="left" imgSrc={landscape}>
-          <div className="slide-text">This is slide {index + 1}</div>
-        </SlideComponent>
+          <SlideComponent className="slider" isActive={activeIndex === index} direction="left">
+            <img src={landscape.image} alt={`Slide image ${index + 1}`} className="slider-image" />
+            <div className="slide-text">
+            {landscape.name}
+              <Button
+                key={index + 1}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                <Link to={"/property"} style={{ textDecoration: 'none', color: 'inherit' }}>
+                  {"view property"}
+                </Link>
+              </Button></div>
+          </SlideComponent>
         ))}
       </div>
       <div className="slider-buttons">
-      <Button variant="text" onClick={prevSlide}>
-        Previous
-      </Button>
-      <Button variant="text" onClick={nextSlide}>
-        Next
-      </Button>
+        <Button variant="text" onClick={prevSlide}>
+          Previous
+        </Button>
+        <Button variant="text" onClick={nextSlide}>
+          Next
+        </Button>
       </div>
     </div>
   );
