ba4a1becb800f327953cd360b8cd44e60224a42e
John <johnclundyy@gmail.com>
2024-02-08 13:12:30 -0700

    Added Services Pages

---
+++ src/pages/Irrigation.js
@@ -0,0 +1,15 @@
+import "../styles/Irrigation.css";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+
+function Irrigation() {
+  return (
+    <div className="Irrigation">
+      <Nav />
+      Irrigation
+      <Footer />
+    </div>
+  );
+}
+
+export default Irrigation;
