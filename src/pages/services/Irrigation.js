8ffc56cfbfe3aac1c3b78c554200cedb1079ce2b
John <johnclundyy@gmail.com>
2024-02-09 11:55:13 -0700

    Service pages are set up

--- src/pages/services/Irrigation.js
+++ src/pages/services/Irrigation.js
@@ -1,12 +1,17 @@
 import "./Services.css";
 import Nav from "../../components/Nav";
 import Footer from "../../components/Footer";
+import service3 from "../../assets/service3.png";
 
 function Irrigation() {
   return (
     <div className="Services">
       <Nav />
-      Irrigation
+      <img src={service3} alt="Irrigation" className="service-image" />
+      <h1 className="services-title">Irrigation</h1>
+      <p className="services-text">
+      Efficient irrigation systems are key to maintaining a vibrant landscape while conserving water and promoting sustainability. We offer advanced irrigation solutions that ensure every plant receives the precise amount of water it needs to thrive. Our systems are engineered to reduce waste, lower utility costs, and provide convenience through automation. With the use of cutting-edge technology, we can create a custom irrigation plan that adapts to your landscape's unique requirements, ensuring lush greenery and blooming gardens with minimal environmental impact. Let us help you cultivate a thriving outdoor space with the gold standard in irrigation efficiency.
+      </p>
       <Footer />
     </div>
   );
--- src/pages/services/Outdoor.js
+++ src/pages/services/Outdoor.js
@@ -1,12 +1,17 @@
 import "./Services.css";
 import Nav from "../../components/Nav";
 import Footer from "../../components/Footer";
+import service2 from "../../assets/service2.png";
 
 function Outdoor() {
   return (
     <div className="Services">
       <Nav />
-      Outdoor
+      <img src={service2} alt="Outdoor Living Spaces" className="service-image" />
+      <h1 className="services-title">Outdoor Living Spaces</h1>
+      <p className="services-text">
+      Transforming your backyard into a serene outdoor living space is an investment in lifestyle and home value. We specialize in creating bespoke outdoor environments that invite relaxation and entertainment. Our designs extend your living area into the open air, incorporating elements such as elegant patios, outdoor kitchens, cozy fire pits, and tranquil water features. Each project is tailored to reflect your personal style and to complement the natural landscape, providing a private retreat for you and your family to unwind, entertain, and create lasting memories in the comfort of your own backyard.
+      </p>
       <Footer />
     </div>
   );
