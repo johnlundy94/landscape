83d8660c65442db15ed208fd4802458c98cdef78
John <johnclundyy@gmail.com>
2024-02-09 09:53:15 -0700

    Eden property's base implementation complete

--- src/pages/properties/Arcadian.js
+++ src/pages/properties/Arcadian.js
@@ -16,48 +16,48 @@ function Arcadian() {
 
   const [activeIndex, setActiveIndex] = useState(0);
 
-  const landscapes = [
-    {image: arcadian1},
-    {image: arcadian2},
-    {image: arcadian3},
-    {image: arcadian4},
-    {image: arcadian5},
+  const properties = [
+    { image: arcadian1 },
+    { image: arcadian2 },
+    { image: arcadian3 },
+    { image: arcadian4 },
+    { image: arcadian5 },
   ];
 
   const nextSlide = () => {
     setActiveIndex((prevIndex) =>
-      prevIndex === landscapes.length - 1 ? 0 : prevIndex + 1
+      prevIndex === properties.length - 1 ? 0 : prevIndex + 1
     );
   };
 
   const prevSlide = () => {
     setActiveIndex((prevIndex) =>
-      prevIndex === 0 ? landscapes.length - 1 : prevIndex - 1
+      prevIndex === 0 ? properties.length - 1 : prevIndex - 1
     );
   };
 
   return (
     <div className="properties-container">
-            <Nav />
-<div className="hero-container">
-      <h1 className="text-span-2">Arcadian Retreat</h1>
-      <div className="slider-container" id="properties" >
-        {landscapes.map((landscape, index) => (
-          <SlideComponent className="slider" isActive={activeIndex === index} direction="left">
-            <img src={landscape.image} alt={`Slide image ${index + 1}`} className="slider-image" />
-          </SlideComponent>
-        ))}
+      <Nav />
+      <div className="hero-container">
+        <h1 className="text-span-2">Arcadian Retreat</h1>
+        <div className="slider-container" id="properties" >
+          {properties.map((property, index) => (
+            <SlideComponent className="slider" isActive={activeIndex === index} direction="left">
+              <img src={property.image} alt={`Slide image ${index + 1}`} className="slider-image" />
+            </SlideComponent>
+          ))}
+        </div>
+        <div className="slider-buttons">
+          <Button variant="text" onClick={prevSlide}>
+            Previous
+          </Button>
+          <Button variant="text" onClick={nextSlide}>
+            Next
+          </Button>
+        </div>
       </div>
-      <div className="slider-buttons">
-        <Button variant="text" onClick={prevSlide}>
-          Previous
-        </Button>
-        <Button variant="text" onClick={nextSlide}>
-          Next
-        </Button>
-      </div>
-    </div>
-    <Footer />
+      <Footer />
     </div>
   );
 }
--- src/pages/properties/Eden.js
+++ src/pages/properties/Eden.js
@@ -1,12 +1,62 @@
-import "../../styles/Eden.css";
+import { useState } from "react";
+import "./Properties.css";
+import SlideComponent from "../../api/SlideComponent";
+import eden1 from "../../assets/eden/eden1.png";
+import eden2 from "../../assets/eden/eden2.png";
+import eden3 from "../../assets/eden/eden3.png";
+import eden4 from "../../assets/eden/eden4.png";
+import eden5 from "../../assets/eden/eden5.png";
+import { Button } from "@mui/material";
 import Nav from "../../components/Nav";
 import Footer from "../../components/Footer";
 
+
+
 function Eden() {
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const properties = [
+    { image: eden1 },
+    { image: eden2 },
+    { image: eden3 },
+    { image: eden4 },
+    { image: eden5 },
+  ];
+
+  const nextSlide = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === properties.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevSlide = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === 0 ? properties.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
-    <div className="Eden">
+    <div className="properties-container">
       <Nav />
-      Eden
+      <div className="hero-container">
+        <h1 className="text-span-2">Eden Enclave</h1>
+        <div className="slider-container" id="properties" >
+          {properties.map((property, index) => (
+            <SlideComponent className="slider" isActive={activeIndex === index} direction="left">
+              <img src={property.image} alt={`Slide image ${index + 1}`} className="slider-image" />
+            </SlideComponent>
+          ))}
+        </div>
+        <div className="slider-buttons">
+          <Button variant="text" onClick={prevSlide}>
+            Previous
+          </Button>
+          <Button variant="text" onClick={nextSlide}>
+            Next
+          </Button>
+        </div>
+      </div>
       <Footer />
     </div>
   );
