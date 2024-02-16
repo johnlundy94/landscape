778b2a7475e2d4bf3ceda242564da7083b198b90
John Lundy <johnclundyy@gmail.com>
2024-02-16 16:50:08 -0700

    Padding adjustment

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -2,7 +2,20 @@ import { useState } from "react";
 import axios from "axios";
 import "../styles/Quote.css";
 import { Link } from "react-router-dom";
-import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button,  FormControl, TextField, Checkbox, FormControlLabel, FormGroup, FormLabel } from '@mui/material';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button,
+  FormControl,
+  TextField,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+} from "@mui/material";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 
@@ -28,53 +41,55 @@ function Quote() {
     address: "",
     budget: "",
     description: "",
-  })
+  });
 
   const [openDialog, setOpenDialog] = useState(false);
 
-  const validateEmail = email => {
-    const regEmial = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regEmial.test(email)
-  }
+  const validateEmail = (email) => {
+    const regEmial =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regEmial.test(email);
+  };
 
   const validatePhone = (phone) => {
     const regPhone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    return regPhone.test(phone)
-  }
+    return regPhone.test(phone);
+  };
 
   const validateField = (name, value) => {
-
     if (typeof value === "boolean") {
       return "";
     }
 
-    switch(name) {
+    switch (name) {
       case "email":
         return validateEmail(value) ? "" : "Invalid email format";
       case "phone":
-        return validatePhone(value) ? "" : "Invalid phone number, please enter 10 digits";
+        return validatePhone(value)
+          ? ""
+          : "Invalid phone number, please enter 10 digits";
       default:
         return value.trim() ? "" : "This field is required";
     }
-  }
+  };
 
   const handleChange = (event) => {
-    const {name, value, checked, type} = event.target;
+    const { name, value, checked, type } = event.target;
     if (type === "checkbox") {
-      setFormData(prevState => ({
+      setFormData((prevState) => ({
         ...prevState,
         services: {
           ...prevState.services,
-          [name]: checked
-        }
-      }))
+          [name]: checked,
+        },
+      }));
     } else {
-      setFormData(prevState => ({
+      setFormData((prevState) => ({
         ...prevState,
         [name]: value,
       }));
     }
-    setFormErrors(prevErrors => ({
+    setFormErrors((prevErrors) => ({
       ...prevErrors,
       [name]: validateField(name, type === "checkbox" ? checked : value),
     }));
@@ -82,8 +97,8 @@ function Quote() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    
-    const newErrors= Object.keys(formData).reduce((acc, key) => {
+
+    const newErrors = Object.keys(formData).reduce((acc, key) => {
       if (key !== "services") {
         acc[key] = validateField(key, formData[key]);
       }
@@ -92,42 +107,43 @@ function Quote() {
 
     setFormErrors(newErrors);
 
-    const hasErrors = Object.values(newErrors).some(error => error);
-    if(hasErrors) {
+    const hasErrors = Object.values(newErrors).some((error) => error);
+    if (hasErrors) {
       console.log("Form has errors");
       return;
     }
 
     const apiGatewayUrl = process.env.REACT_APP_API_GATEWAY_URL;
 
-    axios.post(apiGatewayUrl, JSON.stringify(formData), {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => {
-      console.log("Success: ", response.data);
-
-      setFormData({
-        name: "",
-        email: "",
-        phone: "",
-        address: "",
-        services: {
-          landscapeDesign: false,
-          outdoorLivingSpaces: false,
-          irrigation: false,
+    axios
+      .post(apiGatewayUrl, JSON.stringify(formData), {
+        headers: {
+          "Content-Type": "application/json",
         },
-        budget: "",
-        description: "",
       })
-      setOpenDialog(true);
-    })
-    .catch(error => {
-      console.log("Error: ", error);
-    });
+      .then((response) => {
+        console.log("Success: ", response.data);
+
+        setFormData({
+          name: "",
+          email: "",
+          phone: "",
+          address: "",
+          services: {
+            landscapeDesign: false,
+            outdoorLivingSpaces: false,
+            irrigation: false,
+          },
+          budget: "",
+          description: "",
+        });
+        setOpenDialog(true);
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
     console.log(formData);
-  }
+  };
 
   return (
     <div className="Quote">
@@ -138,21 +154,29 @@ function Quote() {
         aria-labelledby="alert-dialog-text"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">{"Submission Successful"}</DialogTitle>
+        <DialogTitle id="alert-dialog-title">
+          {"Submission Successful"}
+        </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
             Thank you for your interest! We will reach out shortly!
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button component={Link} to="/" color="primary" autoFocus onClick={() => setOpenDialog(false)}>
+          <Button
+            component={Link}
+            to="/"
+            color="primary"
+            autoFocus
+            onClick={() => setOpenDialog(false)}
+          >
             Go to Home
           </Button>
         </DialogActions>
       </Dialog>
       <h1 className="quote-title">Reach Out To Us!</h1>
       <div className="quote-form">
-        <FormControl component="form" onSubmit={handleSubmit} sx={{ m: 4 }} >
+        <FormControl component="form" onSubmit={handleSubmit} sx={{ m: 4 }}>
           <TextField
             label="Name"
             variant="outlined"
@@ -200,15 +224,33 @@ function Quote() {
           <FormLabel component="legend">Services Interested In</FormLabel>
           <FormGroup>
             <FormControlLabel
-              control={<Checkbox checked={formData.services.landscapeDesign} onChange={handleChange} name="landscapeDesign" />}
+              control={
+                <Checkbox
+                  checked={formData.services.landscapeDesign}
+                  onChange={handleChange}
+                  name="landscapeDesign"
+                />
+              }
               label="Landscape Design"
             />
             <FormControlLabel
-              control={<Checkbox checked={formData.services.outdoorLivingSpaces} onChange={handleChange} name="outdoorLivingSpaces" />}
+              control={
+                <Checkbox
+                  checked={formData.services.outdoorLivingSpaces}
+                  onChange={handleChange}
+                  name="outdoorLivingSpaces"
+                />
+              }
               label="Outdoor Living Spaces"
             />
             <FormControlLabel
-              control={<Checkbox checked={formData.services.irrigation} onChange={handleChange} name="irrigation" />}
+              control={
+                <Checkbox
+                  checked={formData.services.irrigation}
+                  onChange={handleChange}
+                  name="irrigation"
+                />
+              }
               label="Irrigation"
             />
           </FormGroup>
@@ -236,7 +278,12 @@ function Quote() {
             margin="normal"
             fullWidth
           />
-          <Button type="submit" variant="contained" color="primary" sx={{ mt: 2 }}>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            sx={{ mt: 2 }}
+          >
             Submit
           </Button>
         </FormControl>
