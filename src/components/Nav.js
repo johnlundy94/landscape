1c5d81ebc91d53357dba74fcf88485d639ad03c3
John Lundy <johnclundyy@gmail.com>
2024-02-16 15:37:22 -0700

    Updated nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,30 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import LawnMower from '@mui/icons-material/Agriculture';
+import * as React from "react";
+import { useLocation } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import LawnMower from "@mui/icons-material/Agriculture";
 import "../styles/Nav.css";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const pages = [
+  { name: "Home", path: "/", type: "link" },
   { name: "Quote", path: "/quote", type: "link" },
-  { name: "Properties", anchor: "properties", type: "anchor" },
-  { name: "Services", anchor: "services", type: "anchor" },
-  { name: "About", anchor: "about", type: "anchor" },
-  { name: "Testimonials", anchor: "testimonials", type: "anchor" }];
+  { name: "Properties", path: "/serenity", type: "link" },
+  { name: "Services", path: "/design", type: "link" },
+];
 
 function Nav() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const location = useLocation();
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -31,13 +34,20 @@ function Nav() {
     setAnchorElNav(null);
   };
 
+  const isHome = location.pathname === "/";
+
   return (
-    <AppBar position="static" color="primary" className='Nav' id="back-to-top-anchor" >
-      <Container maxWidth="xl" >
+    <AppBar
+      position="static"
+      color="primary"
+      className="Nav"
+      id="back-to-top-anchor"
+    >
+      <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {/* Logo Picture Desktop */}
-            <LawnMower sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <LawnMower sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
             {/* Logo Title Desktop */}
             <Typography
               variant="h6"
@@ -46,21 +56,20 @@ function Nav() {
               href="/"
               sx={{
                 mr: 2,
-                display: { xs: 'none', md: 'flex' },
-                fontFamily: 'monospace',
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
                 fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
               }}
             >
               Verdant
             </Typography>
           </Box>
 
-
           {/* Logo Picture Phone */}
-          <LawnMower sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <LawnMower sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
           {/* Logo Title Phone */}
           <Typography
             variant="h5"
@@ -69,20 +78,20 @@ function Nav() {
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             Verdant
           </Typography>
 
           {/* Hamburger */}
-          <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -97,34 +106,39 @@ function Nav() {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
               {pages.map((page) => {
                 const menuItemContent = (
                   <Typography textAlign="center">{page.name}</Typography>
-                )
+                );
 
                 return (
                   <MenuItem key={page.name} onClick={handleCloseNavMenu}>
-                    {page.type === "link" ? (
-                      <Link to={page.path} style={{ textDecoration: "none", color: "#272727" }}>
+                    {isHome || page.type === "link" ? (
+                      <Link
+                        to={page.path}
+                        style={{ textDecoration: "none", color: "#272727" }}
+                      >
                         {menuItemContent}
                       </Link>
                     ) : (
-                      <a href={`#${page.anchor}`} 
-                      style={{ textDecoration: "none", color: "inherit" }}>
+                      <a
+                        href={`#${page.anchor}`}
+                        style={{ textDecoration: "none", color: "inherit" }}
+                      >
                         {menuItemContent}
                       </a>
                     )}
@@ -135,19 +149,25 @@ function Nav() {
           </Box>
 
           {/* Nav links desktop */}
-          <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 0, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page.name}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: "white", display: "block" }}
               >
-                {page.type === 'link' ? (
-                  <Link to={page.path} style={{ textDecoration: 'none', color: 'inherit' }}>
+                {isHome || page.type === "link" ? (
+                  <Link
+                    to={page.path}
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
                     {page.name}
                   </Link>
                 ) : (
-                  <a href={`#${page.anchor}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                  <a
+                    href={`#${page.anchor}`}
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
                     {page.name}
                   </a>
                 )}
