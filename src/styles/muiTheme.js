260ee8a055ae1a1d2aaa593a6d878fa5c7c66a86
John Lundy <johnclundyy@gmail.com>
2024-02-19 11:11:16 -0700

    Fixed the color of the form

--- src/styles/muiTheme.js
+++ src/styles/muiTheme.js
@@ -3,25 +3,62 @@ import { createTheme } from "@mui/material/styles";
 const muiTheme = createTheme({
   palette: {
     primary: {
-      main: '#272727',
-      light: '#4d4d4d',
-      dark: '#1c1c1c',
-      contrastText: '#fbfbfb',
+      main: "#272727",
+      light: "#4d4d4d",
+      dark: "#1c1c1c",
+      contrastText: "#fbfbfb",
     },
     text: {
-      primary: '#fbfbfb', 
-      secondary: '#fbfbfb',
+      primary: "#fbfbfb",
+      secondary: "#fbfbfb",
     },
   },
   components: {
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          // Label color when not focused
+          color: "#fbfbfb",
+          "&.Mui-focused": {
+            // Label color when focused
+            color: "#fbfbfb",
+          },
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#fbfbfb", // Change the border color
+          },
+          "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#fbfbfb", // Change the border color on hover
+          },
+          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#fbfbfb", // Change the border color when focused
+          },
+        },
+      },
+    },
+    MuiCheckbox: {
+      styleOverrides: {
+        root: {
+          color: "#fbfbfb", // Set the default color for the checkbox
+          "&.Mui-checked": {
+            color: "#fbfbfb", // Ensure the checkbox color remains the same when checked
+          },
+        },
+      },
+    },
     MuiCard: {
       styleOverrides: {
         root: {
-          borderRadius: 30, 
-          backgroundColor: '#272727',
-          color: '#fbfbfb',
-          '& .MuiTypography-colorTextSecondary': {
-            color: '#fbfbfb',
+          borderRadius: 30,
+          backgroundColor: "#272727",
+          color: "#fbfbfb",
+          "& .MuiTypography-colorTextSecondary": {
+            color: "#fbfbfb",
           },
         },
       },
@@ -29,21 +66,21 @@ const muiTheme = createTheme({
     MuiTypography: {
       styleOverrides: {
         root: {
-          color: '#fbfbfb',
+          color: "#fbfbfb",
         },
       },
     },
     MuiButton: {
       styleOverrides: {
-        text: { 
-          color: '#fbfbfb', 
+        text: {
+          color: "#fbfbfb",
         },
       },
     },
     MuiMenu: {
       styleOverrides: {
         paper: {
-          backgroundColor: '#272727',
+          backgroundColor: "#272727",
         },
       },
     },
